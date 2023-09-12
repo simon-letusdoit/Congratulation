5f048e6392de3f69260d1e8a4d24ccac367f1a39
Simon LetUsDoIT <simon@letusdoit.dk>
2023-09-12 19:52:47 +0200

    Cleanup as per sonarcloud

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,24 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Congratulations.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
+            //not used
 
         }
     }
